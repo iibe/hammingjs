1cea1973c57a7fb7fcde1912870d3934689de63e
iibe <inctonum@gmail.com>
2021-11-21 09:19:59 +0300

    feat: add error detection mechanism

--- index.js
+++ index.js
@@ -1,51 +1,101 @@
-function encode(keyword) {
-  if (typeof keyword !== "string" || keyword.match(/[^10]/)) {
-    throw new Error("Keyword should be a binary string. E.g 101011");
+function encode(bitword) {
+  if (bitword.match(/[^10]/) && bitword.length > 0) {
+    throw new Error("It should be a binary string. E.g 101011");
   }
 
-  // calculate control bits positions
+  // find control bits positions
   const indexes = [];
   while (
-    indexes.length < Math.ceil(Math.log2(keyword.length + 1 + indexes.length))
+    indexes.length < Math.ceil(Math.log2(bitword.length + indexes.length + 1))
   ) {
     indexes.push(2 ** indexes.length - 1);
   }
 
-  // insert control bits in keyword
-  for (let index of indexes) {
-    keyword = insert(keyword, index, "0");
+  // insert control bits in bitword
+  for (const index of indexes) {
+    bitword = insert(bitword, index, "0");
   }
 
-  // create a table of transformation
-  const table = [keyword];
-  for (let index of indexes) {
-    // create specific row for i-th control bit
-    table.push(replace(keyword, index, "1"));
+  // initialize a table of transformation
+  const table = [bitword];
+  for (const _ of indexes) {
+    table.push("");
   }
 
-  // place col number in binary format at according col position
-  for (let col = 0; col < keyword.length; col++) {
-    const binary = ((col + 1) >>> 0).toString(2).padStart(indexes.length, "0");
+  // put col number (in binary format) at according col position
+  for (let col = 0; col < bitword.length; col++) {
+    const bin = ((col + 1) >>> 0).toString(2).padStart(indexes.length, "0");
 
-    for (let row = 1; row <= indexes.length; row++) {
-      table[row] = replace(table[row], col, binary[binary.length - row]);
+    for (let row = 1; row < indexes.length + 1; row++) {
+      table[row] = table[row].concat(bin[bin.length - row]);
     }
   }
 
   // calculate matches with control word
-  for (let i = 1; i < table.length; i++) {
+  for (let row = 1; row < table.length; row++) {
     let match = 0;
-    for (let j = 0; j < table[0].length; j++) {
-      if (table[0][j] === "1" && table[i][j] === table[0][j]) match++;
+    for (let col = 0; col < table[0].length; col++) {
+      if (table[0][col] === "1" && table[0][col] === table[row][col]) {
+        match++;
+      }
     }
 
-    keyword = replace(keyword, indexes[i - 1], (match % 2).toString());
+    bitword = replace(bitword, indexes[row - 1], (match % 2).toString());
   }
 
-  return keyword;
+  return bitword;
 }
 
-console.log(encode("100100101110001"));
+function decode(bitword) {
+  if (bitword.match(/[^10]/) && bitword.length > 0) {
+    throw new Error("It should be a binary string. E.g 101011");
+  }
+
+  // find control bits positions
+  const indexes = [0, 1];
+  while (2 ** indexes.length < bitword.length) {
+    indexes.push(2 ** indexes.length - 1);
+  }
+
+  // initialize a table of transformation
+  const table = [bitword];
+  for (const _ of indexes) {
+    table.push("");
+  }
+
+  // put col number (in binary format) at according col position
+  for (let col = 0; col < bitword.length; col++) {
+    const bin = ((col + 1) >>> 0).toString(2).padStart(indexes.length, "0");
+
+    for (let row = 1; row < indexes.length + 1; row++) {
+      table[row] = table[row].concat(bin[bin.length - row]);
+    }
+  }
+
+  // calculate matches with control word
+  const error = [];
+  for (let row = 1; row < table.length; row++) {
+    let match = 0;
+    for (let col = 0; col < table[0].length; col++) {
+      if (table[0][col] === "1" && table[0][col] === table[row][col]) {
+        match++;
+      }
+    }
+
+    error.push(match % 2);
+  }
+
+  return parseInt(error.reverse().join(""), 2);
+}
+
+const v = "100100101110001"; // initial value
+const i = encode(v); // encoded input
+const e = replace(i, 5, "1"); // 6th bit error
+const o = decode(e); // detected error position
+console.log("V:", v);
+console.log("I:", i);
+console.log("E:", e);
+console.log("O:", o);
 
 /**
  *
